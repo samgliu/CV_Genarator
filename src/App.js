674b8b0d0ac9ab82dcf7fbceef6699a2606b61ec
samgliu <icefire587@gmail.com>
2021-10-11 23:17:24 -0400

    v1.2 fix typo

--- src/App.js
+++ src/App.js
@@ -296,7 +296,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <h1>CV Genarator</h1>
+                <h1>CV Generator</h1>
                 <Line />
                 <Information
                     info={this.state.information}
